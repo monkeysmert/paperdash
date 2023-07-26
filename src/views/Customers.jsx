d8656f7b440d50749e8cb178a3fb18c5ebc9cd7d
mert <mert@monkeysteam.com>
2023-07-26 19:52:00 +0300

    first commit

---
+++ src/views/Customers.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactTables from "./tables/ReactTables"
+
+const Customers = () => {
+  return (
+    <ReactTables />
+  )
+}
+
+export default Customers
---
+++ src/views/Nurses.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactTables from "./tables/ReactTables"
+
+const Nurses = () => {
+  return (
+    <ReactTables />
+  )
+}
+
+export default Nurses
